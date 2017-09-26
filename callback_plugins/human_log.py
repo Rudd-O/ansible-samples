cc60d3890e8840290115252023e292463945d6ef
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2017-09-26 13:41:28 +0200

    Fix Ansible human log plugin.

--- callback_plugins/human_log.py
+++ callback_plugins/human_log.py
@@ -11,8 +11,10 @@ from ansible.parsing.yaml.dumper import AnsibleDumper
 from ansible.plugins.callback.default import CallbackModule as CallbackModule_default
 from ansible.module_utils._text import to_bytes
 from ansible.utils.color import stringc
-from ansible.vars.unsafe_proxy import AnsibleUnsafeText
-
+try:
+   from ansible.vars.unsafe_proxy import AnsibleUnsafeText
+except ImportError:
+   from ansible.utils.unsafe_proxy import AnsibleUnsafeText
 
 class LiteralText(unicode): pass
 
