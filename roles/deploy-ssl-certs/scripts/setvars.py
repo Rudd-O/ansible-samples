160509b67232afe4f126cdcc286504225e365be7
Rudd-O <rudd-o@rudd-o.com>
2020-07-07 23:46:38 +0000

    fix

--- roles/deploy-ssl-certs/scripts/setvars.py
+++ roles/deploy-ssl-certs/scripts/setvars.py
@@ -7,27 +7,34 @@ import json
 
 i = json.loads(sys.argv[1])
 
-safeify = lambda x: x.replace(os.path.sep, "_").replace(os.path.pardir, "_" * len(os.path.pardir))
+safeify = lambda x: x.replace(os.path.sep, "_").replace(
+    os.path.pardir, "_" * len(os.path.pardir)
+)
 incertdir = lambda x: os.path.join(i["options"]["certificates_dir"], x)
 inkeydir = lambda x: os.path.join(i["options"]["keys_dir"], x)
 
 o = {}
-for k, v in i.items():
-    if k == "options": continue
+for k, v in list(i.items()):
+    if k == "options":
+        continue
     o[k] = {}
     o[k]["key"] = {}
     o[k]["key"]["path"] = inkeydir(safeify(k) + ".key")
-    o[k]["key"]["data"] = v['key']
+    o[k]["key"]["data"] = v["key"]
     o[k]["user_may_read"] = v.get("user_may_read", "")
     o[k]["certificate"] = {}
     o[k]["certificate"]["path"] = incertdir(safeify(k) + ".crt")
-    o[k]["certificate"]["data"] = v['certificate']
+    o[k]["certificate"]["data"] = v["certificate"]
     o[k]["assembled_certificate"] = {}
-    o[k]["assembled_certificate"]["path"] = incertdir("assembled_" + safeify(k) + ".crt")
+    o[k]["assembled_certificate"]["path"] = incertdir(
+        "assembled_" + safeify(k) + ".crt"
+    )
     o[k]["intermediate_certificates"] = []
-    for n, int in enumerate(v['intermediates']):
+    for n, int in enumerate(v["intermediates"]):
         o[k]["intermediate_certificates"].append({})
-        o[k]["intermediate_certificates"][-1]["path"] = incertdir(("intermediate_%s_" % n) + safeify(k) + ".crt")
+        o[k]["intermediate_certificates"][-1]["path"] = incertdir(
+            ("intermediate_%s_" % n) + safeify(k) + ".crt"
+        )
         o[k]["intermediate_certificates"][-1]["data"] = int
 
-print json.dumps(o),
+print(json.dumps(o), end=" ")
